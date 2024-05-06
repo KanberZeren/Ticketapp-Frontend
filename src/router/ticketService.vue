5d3ce9f961d9f1b28d2e77e6a02cca2ba4013eb5
KanberZeren <zeren.kanber@student.htw-berlin.de>
2024-05-06 22:22:15 +0200

    TicketListe anzeigen im Frontend inkl. Such-buttons und Filter-Regler

---
+++ src/router/ticketService.vue
@@ -0,0 +1,24 @@
+<script>
+import axios from 'axios'
+
+export default {
+  getAllTickets () {
+    return axios.get('http://localhost:8080/tickets')
+  },
+  getTicket: async (ticketId) => {
+    return await axios.get(`http://localhost:8080/ticket/${ticketId}`)
+  },
+  updateTicket: async (ticketId, updatedTicket) => {
+    return await axios.put(`http://localhost:8080/ticket/${ticketId}`, updatedTicket)
+  },
+  deleteTicket (ticketId) {
+    return axios.delete(`http://localhost:8080/ticket/${ticketId}`)
+  },
+  getTicketByTicketnummer (ticketnummer) {
+    return axios.get(`http://localhost:8080/ticketByTicketnummer/${ticketnummer}`)
+  }
+}
+</script>
+
+<style scoped>
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,51 +1,21 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HomePage msg="Welcome to our Ticket Application!"/>
-    <button @click="testCommunication">Test Kommunikation</button>
+    <HomePage msg="Welcome to our Ticketsystem!"/>
+    <TicketListe/>
   </div>
 </template>
 
 <script>
 
 import HomePage from '@/components/HomePage.vue'
-import axios from 'axios'
+// import axios from 'axios'
+import TicketListe from '@/components/TicketListe.vue'
 
 export default {
   name: 'HomeView',
   components: {
-    HomePage
-  },
-  data () {
-    return {
-      employeeList: []
-    }
-  },
-  methods: {
-    testCommunication () {
-      axios.get('http://localhost:8080/test')
-        .then(response => {
-          console.log('Kommunikationstest erfolgreich:', response.data)
-          alert(response.data)
-        })
-        .catch(error => {
-          console.error('Fehler bei der Kommunikationstest-Anfrage', error)
-          alert('Fehler bei der Kommunikation mit dem Backend!')
-        })
-    },
-    fetchEmployeeData (id) {
-      axios.get(`http://localhost:8080/mitarbeiter/${id}`)
-        .then(response => {
-          if (response.data) {
-            this.employeeList = [response.data]
-          } else {
-            console.log('Kein Mitarbeiter mit dieser ID')
-          }
-        })
-        .catch(error => {
-          console.error('Fehler beim Abrufen der Mitarbeiterdaten', error)
-        })
-    }
+    HomePage,
+    TicketListe
   }
 }
 </script>
