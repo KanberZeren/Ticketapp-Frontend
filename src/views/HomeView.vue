87deec478a98245570b831a0bdee0866d23194e4
KanberZeren <zeren.kanber@student.htw-berlin.de>
2024-05-08 01:03:33 +0200

    Registration Page anknüpfung und weitere View-Design anpassungen

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -4,6 +4,7 @@
       <router-view></router-view>
       <HomePage msg="Welcome to our Ticketsystem!"/>
       <TicketListe/>
+      <FooterBar/>
     </div>
   </div>
 </template>
@@ -14,10 +15,12 @@ import HomePage from '@/components/HomePage.vue'
 // import axios from 'axios'
 import TicketListe from '@/components/TicketListe.vue'
 // import NavBar from '@/components/NavBar.vue'
+import FooterBar from '@/components/FooterBar.vue'
 
 export default {
   name: 'HomeView',
   components: {
+    FooterBar,
     HomePage,
     TicketListe
   },
